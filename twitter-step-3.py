30760245074c8d92d249691fd1417135b03355fd
hamidreza khosravi <37435818+hamidreza7966@users.noreply.github.com>
2018-04-08 17:20:07 +0430

    my first commit

--- twitter-step-3.py
+++ twitter-step-3.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-import twitter_config
 import os,fnmatch,codecs
 
-from collections import OrderedDict
 
+counter = 0
 wordcount={}
 
 for dirpath, dirs, files in os.walk('step2'):
     for filename in fnmatch.filter(files, '*.txt'):
-
         with codecs.open(os.path.join(dirpath, filename),'r',encoding="utf-8") as file:
 
             for word in file.read().split():
@@ -16,17 +16,14 @@ for dirpath, dirs, files in os.walk('step2'):
                 else:
                     wordcount[word] += 1
 
+f=open("step3/word-frequency.txt", 'w', encoding="utf-8")
 
-# b = OrderedDict(sorted(wordcount.items(), key=lambda t: t[1]))
-s = [(k, wordcount[k]) for k in sorted(wordcount, key=wordcount.get, reverse=True)]
-
-for k,v in s:
-    # print (k,v)
-    with codecs.open("step3/word-frequency.txt", 'a+', encoding="utf-8") :
-        my_string = u"{key}:{value}: ".format(key=k, value=v)
+for k,v in wordcount.items():
+      #print (k,v)
+        my_string = u"{key}:{value} ".format(key=k, value=v)
         print(my_string)
-        f.write(my_string)
-
-# codecs.open("step2/" + filename, 'w', encoding="utf-8") as f:
-#     f.write(tweet)
+        f.write(my_string+"\n")
+f.close();
 file.close();
+
+# with codecs.open("step3/word-frequency.txt", 'a+', encoding="utf-8") as f:
